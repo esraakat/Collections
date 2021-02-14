e2035921fae79ef982e5bab0cd36fa3042d5fa48
esra akat <383196@ogr.ktu.edu.tr>
2021-02-14 23:21:47 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Engin", "Halil", "Murat", "Kerem" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            Console.WriteLine(names[3]);
+
+            names = new string[5];
+            names[4] = "İlker";
+            Console.WriteLine(names[4]);
+            Console.WriteLine(names[0]); //empty element
+
+            List<string> names2 = new List<string> { "Engin", "Murat", "Halil", "Kerem" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("İlker");
+            Console.WriteLine(names2[4]);
+
+            List<string> names3 = new List<string>(); //its alternative
+            names3.Add("Engin");
+            names3.Add("Murat");
+            names3.Add("Halil");
+            names3.Add("Kerem");
+            names3.Add("İlker");
+            Console.WriteLine(names3[0]);
+            Console.WriteLine(names3[1]);
+            Console.WriteLine(names3[2]);
+            Console.WriteLine(names3[3]);
+            Console.WriteLine(names3[4]);
+
+
+        }
+    }
+}
